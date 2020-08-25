00f28be8576fd929d2889926092a8b5c1d02f505
kashaf12 <kashafaahmed@gmail.com>
2020-08-25 18:49:50 +0530

    added new changes

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE,{ useNewUrlParser: true });
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
