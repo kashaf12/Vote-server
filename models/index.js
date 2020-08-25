f46859ce419421b5d40c45fa00895d3b2a4ea859
kashaf12 <kashafaahmed@gmail.com>
2020-08-25 18:32:19 +0530

    node build

--- models/index.js
+++ models/index.js
@@ -3,14 +3,5 @@ mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE);
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Admin:<password>@cluster0-lfzvm.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
